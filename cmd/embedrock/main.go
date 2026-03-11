232979a375fec58d8291a792c4f61f9d1647ec6c
Loki@FastStart <loki@faststart.internal>
2026-03-11 22:17:03 +0000

    Fix: add cmd/embedrock/main.go (was ignored by .gitignore)

---
+++ cmd/embedrock/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/inceptionstack/embedrock"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+func main() {
+	port := flag.Int("port", 8089, "Port to listen on")
+	host := flag.String("host", "127.0.0.1", "Host to bind to")
+	region := flag.String("region", "us-east-1", "AWS region for Bedrock")
+	model := flag.String("model", "amazon.titan-embed-text-v2:0", "Default Bedrock embedding model")
+	showVersion := flag.Bool("version", false, "Show version and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("embedrock %s (commit: %s, built: %s)\n", version, commit, date)
+		os.Exit(0)
+	}
+
+	emb, err := embedrock.NewBedrockEmbedder(*region, *model)
+	if err != nil {
+		log.Fatalf("Failed to create Bedrock embedder: %v", err)
+	}
+
+	handler := embedrock.NewHandler(emb)
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+
+	log.Printf("embedrock %s starting on http://%s (region=%s, model=%s)", version, addr, *region, *model)
+	if err := http.ListenAndServe(addr, handler); err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
+}
